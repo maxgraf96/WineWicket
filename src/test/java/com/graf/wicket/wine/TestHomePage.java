7477e71a06c9a6478c4fc3094ce2bfddc7f28c5a
Max <max.graf@live.at>
2015-09-01 17:19:54 +0200

    Initial commit

---
+++ src/test/java/com/graf/wicket/wine/TestHomePage.java
@@ -0,0 +1,29 @@
+package com.graf.wicket.wine;
+
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestHomePage
+{
+	private WicketTester tester;
+
+	@Before
+	public void setUp()
+	{
+		tester = new WicketTester(new WineApplication());
+	}
+
+	@Test
+	public void homepageRendersSuccessfully()
+	{
+		//start and render the test page
+		tester.startPage(WineHome.class);
+
+		//assert rendered page class
+		tester.assertRenderedPage(WineHome.class);
+	}
+}
